8eb96e673c9edac0b7a99b10b3cba8379640a30a
Shibo Lyu <laosb@lns.unihubs.com>
2020-04-26 22:49:22 +0800

    fix: command match.

--- api/_handlers/command.ts
+++ api/_handlers/command.ts
@@ -2,18 +2,10 @@ import { CQHTTPPostPayload } from '../_types'
 import handlePurge from './purgeGroup'
 
 export default async (payload: CQHTTPPostPayload) => {
-  switch (payload.message.slice(1)) {
-    case 'ping':
-      return 'pong @ ' + new Date().toLocaleString()
-    case '乒':
-      return '乓 @ ' + new Date().toLocaleString()
+  if (payload.message.match(/ping|乒/i))
+    return 'pong @ ' + new Date().toLocaleString()
 
-    case 'purge':
-      return await handlePurge(payload)
-    case '清群':
-      return await handlePurge(payload)
+  if (payload.message.match(/purge|清群/i)) return await handlePurge(payload)
 
-    default:
-      return null
-  }
+  return null
 }
